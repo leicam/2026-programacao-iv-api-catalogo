8d04c2526cff8c1826268c7b1c4fa4904d24e014
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2026-03-09 22:23:53 -0300

    feat: aula 09/03/2026

---
+++ umfgcloud.loja.service/umfgcloud.loja.aplicacao.service/Classes/UsuarioServico.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using umfgcloud.loja.dominio.service.DTO;
+using umfgcloud.loja.dominio.service.Interfaces.Servicos;
+
+namespace umfgcloud.loja.aplicacao.service.Classes
+{
+    public sealed class UsuarioServico : IUsuarioServico
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsuarioServico(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+        }
+
+        public Task<UsuarioDTO.SingInResponse> AutenticarAsync(UsuarioDTO.SingInRequest dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task CadastrarAsync(UsuarioDTO.SingUpRequest dto)
+        {
+            var identityUser = new IdentityUser()
+            {
+                UserName = dto.Email,
+                Email = dto.Email,
+                EmailConfirmed = true,
+            };
+
+            var resultado = await _userManager.CreateAsync(identityUser, dto.Password);
+
+            if (!resultado.Succeeded && resultado.Errors.Any())
+                throw new InvalidOperationException(resultado.Errors.Select(x => x.Description).ToString());
+
+            await _userManager.SetLockoutEnabledAsync(identityUser, true);
+        }
+    }
+}
