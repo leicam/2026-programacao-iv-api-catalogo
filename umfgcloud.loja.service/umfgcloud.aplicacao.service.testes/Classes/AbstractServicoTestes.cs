e501c6e28e95754d10c76ee79d84039db6bd942e
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2026-04-13 20:34:09 -0300

    feat: aula 13/04/2026 parte 1

---
+++ umfgcloud.loja.service/umfgcloud.aplicacao.service.testes/Classes/AbstractServicoTestes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umfgcloud.aplicacao.service.testes.Classes
+{
+    public abstract class AbstractServicoTestes
+    {
+        private const string C_ROLE_DEFAULT = "default-role";
+        private const string C_AUTHENTICATION_AUDIENCE = "altas.mars.net.br";
+        private const string C_AUTHENTICATION_ISSUER = "marscloud.atlas.service";
+
+        private const string C_AUTHORIZATION = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+            "eyJtb2R1bGVzIjoiVG9kb3MiLCJzdWIiOiI0NTUzZmMwNi1mYWM2LTQwMmMtOWVmYy05MDA0ZDhjMjE3" +
+            "ZmIiLCJuYW1laWQiOiI2ZTQ3NWFiMS05ODY5LTQ0NWItODJiNy1kMWNkMmZjMmQzMTQiLCJuYW1lIjoi" +
+            "TUFSUyIsImVtYWlsIjoiTUFSUy5URUMuQlJAR01BSUwuQ09NIiwianRpIjoiYTA0NmQ3MzQtZjUxOC00" +
+            "MTFiLTg1MjAtZjhjYzlmMGYwMThkIiwibmJmIjoxNzczNDA5OTA1LCJpYXQiOjE3NzM0MDk5MDUsInJv" +
+            "bGUiOiJBZG1pbmlzdHJhZG9yIiwiZXhwIjoxNzczNDEzNTA1LCJpc3MiOiJtYXJzY2xvdWQuYXRsYXMu" +
+            "c2VydmljZSIsImF1ZCI6ImF0bGFzLm1hcnMubmV0LmJyIn0.Yb1CXbTIl93aeOqCP9ioWrLk09hP0Q65" +
+            "2l2A4HzDpGw";
+
+        protected const string C_USER_PASSWORD = "123Mudar@";
+    }
+}
--- umfgcloud.loja.service/umfgcloud.infraestrutura.service/Context/MySqlDataBaseContext.cs
+++ umfgcloud.loja.service/umfgcloud.infraestrutura.service/Context/MySqlDataBaseContext.cs
@@ -12,6 +12,9 @@ namespace umfgcloud.infraestrutura.service.Context;
 
 public sealed class MySqlDataBaseContext : IdentityDbContext
 {
+    private const string C_IN_MEMORY_PROVIDER
+        = "Microsoft.EntityFrameworkCore.InMemory";
+
     public MySqlDataBaseContext(DbContextOptions<MySqlDataBaseContext> options)
         : base(options)
     {
@@ -29,7 +32,13 @@ public sealed class MySqlDataBaseContext : IdentityDbContext
 
     private void ApplyMigrations()
     {
+        if (IsInMemory())
+            return;
+
         if (Database.GetPendingMigrations().Any())
             Database.Migrate();
     }
+
+    private bool IsInMemory()
+        => C_IN_MEMORY_PROVIDER.Equals(Database.ProviderName);
 }
